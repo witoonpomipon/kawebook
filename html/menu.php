292ce2b11d835a143c89f613d4e3925c4082bafc
wit <wiittest1997@gmail.com>
2020-08-14 12:22:50 +0700

    Revert "Update menu.php"
    This reverts commit bb7235b93f458dbe3e393a5a9b8144e03d661af4.

--- html/menu.php
+++ html/menu.php
@@ -4,7 +4,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
 <?php include('headerfile.php'); ?>
-<?php echo 'testing'; ?>
 </head>
 <body>
 <div class="menu-header row-eq-height">
